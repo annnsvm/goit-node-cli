53ac50c9b9cc10c8fe40eebdcfe0b6708ca148a4
Anna Samoilova <samoilova0299@gmail.com>
2025-07-01 22:42:07 +0200

    add fix

--- contacts.js
+++ contacts.js
@@ -2,7 +2,7 @@ import { readFile, writeFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { nanoid } from 'nanoid';
 
-const contactsPath = resolve('src', 'db', 'contacts.json');
+const contactsPath = resolve('db', 'contacts.json');
 
 console.log(contactsPath);
 
